65776becd2275e87eb2e074559337229fc9d7641
Robert Grinshtein <grinshtein.ra21@physics.msu.ru>
2026-01-29 15:17:10 +0300

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/skalyk/', // <--- ДОБАВЬТЕ ЭТУ СТРОЧКУ! (Замените skalyk на имя вашего репозитория, если оно другое)
   resolve: {
     alias: {
-      // Это заставляет все библиотеки использовать одну и ту же копию React
       react: path.resolve(__dirname, './node_modules/react'),
       'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
     },
